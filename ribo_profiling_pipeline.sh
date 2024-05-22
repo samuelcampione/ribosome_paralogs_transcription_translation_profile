83247ab6227781e47bc409a069d68c457f3c42e8
Samuel Campione <stcampione@dons.usfca.edu>
2024-05-22 16:05:11 -0700

    update pipelines

--- ribo_profiling_pipeline.sh
+++ ribo_profiling_pipeline.sh
@@ -52,147 +52,97 @@ while IFS= read -r srr; do
 
 
 
-    # #######################
-    # # Step 2
-    # #######################
-    # echo "2. Trim adapters"
+    #######################
+    # Step 2
+    #######################
+    echo "2. Trim adapters"
 
-    # # Trim adapter and length (Trim 5nt from the 5' and 4nt from the 3' end)
+    # Trim adapter and length (Trim 5nt from the 5' and 4nt from the 3' end)
+    cutadapt --report minimal \
+             --cores 8 \
+             -a file:/Users/scampione/data/unique_sequencing_adapters.fasta \
+             -u 5 -u -4 \
+             --minimum-length 25 \
+             -o trimmed_adapter_${srr}.fastq \
+             ${srr}_1.fastq
+
     # cutadapt --report minimal \
     #          --cores 8 \
-    #          -a file:/Users/scampione/data/unique_sequencing_adapters.fasta \
+    #          -a file:/Users/scampione/data/ribo_seq_adapters.fasta \
     #          -u 5 -u -4 \
     #          --minimum-length 25 \
     #          -o trimmed_adapter_${srr}.fastq \
     #          ${srr}_1.fastq
 
-    # # cutadapt --report minimal \
-    # #          --cores 8 \
-    # #          -a file:/Users/scampione/data/ribo_seq_adapters.fasta \
-    # #          -u 5 -u -4 \
-    # #          --minimum-length 25 \
-    # #          -o trimmed_adapter_${srr}.fastq \
-    # #          ${srr}_1.fastq
+
+
+    # Trim polyG 
+    cutadapt --report minimal \
+             -a GGGGGG \
+             --minimum-length 25 \
+             -o trimmed_${srr}.fastq trimmed_adapter_${srr}.fastq
+
+
+    # Remove reads with unknown bases NNNNNNNNN
+    fastp -i trimmed_${srr}.fastq -o filtered_${srr}.fastq --n_base_limit 0
+
+    echo "Running quality check report on trimmed filtered reads"    
+    fastqc -t 8 filtered_${srr}.fastq
 
 
 
-    # # Trim polyG 
-    # cutadapt --report minimal \
-    #          -a GGGGGG \
-    #          --minimum-length 25 \
-    #          -o trimmed_${srr}.fastq trimmed_adapter_${srr}.fastq
-
-
-    # # Remove reads with unknown bases NNNNNNNNN
-    # fastp -i trimmed_${srr}.fastq -o filtered_${srr}.fastq --n_base_limit 0
-
-    # echo "Running quality check report on trimmed filtered reads"    
-    # fastqc -t 8 filtered_${srr}.fastq
-
-
-
-    # # Ribosomal RNA removal (output is non-rRNA reads)
+    # Ribosomal RNA removal (output is non-rRNA reads)
     
-    # echo "Aligning against rRNA_Saccharomyces_cerevisiae_index"
-    # bowtie2 -x /Users/scampione/data/rRNA_Saccharomyces_cerevisiae_index/rRNA.Saccharomyces_cerevisiae \
-    #         -p 8 \
-    #         -U filtered_${srr}.fastq \
-    #         -S rrna_mapped_${srr}.sam \
-    #         --un non_rrna_reads_${srr}.fastq
+    echo "Aligning against rRNA_Saccharomyces_cerevisiae_index"
+    bowtie2 -x /Users/scampione/data/rRNA_Saccharomyces_cerevisiae_index/rRNA.Saccharomyces_cerevisiae \
+            -p 8 \
+            -U filtered_${srr}.fastq \
+            -S rrna_mapped_${srr}.sam \
+            --un non_rrna_reads_${srr}.fastq
 
 
-    # # Don't use reads under 25 nt
-    # seqtk seq -L 25 non_rrna_reads_${srr}.fastq > filtered_non_rrna_reads_${srr}.fastq
+    # Don't use reads under 25 nt
+    seqtk seq -L 25 non_rrna_reads_${srr}.fastq > filtered_non_rrna_reads_${srr}.fastq
 
-    # echo "Running quality check report on non_rrna_reads_${srr}.fastq"
-    # fastqc -q -t 8 filtered_non_rrna_reads_${srr}.fastq
-
-
-
-    # ########################
-    # # Step 3
-    # ########################
-    # echo "4. Alignment and quantification of non rRNA reads"
-
-    # echo "Aligning against s_cerevisiae_R64 using bowtie 2"
-
-    # # Build updated reference genome RF64-4-1
-    # # bowtie2-build /Users/scampione/data/s_cer_r64_4_1_bowtie2_index/GCF_000146045.2_R64_genomic.fna \
-    # #               /Users/scampione/data/s_cer_r64_4_1_bowtie2_index/s_cer_r64_4_1
-
-    # bowtie2 -x /Users/scampione/data/s_cer_r64_4_1_bowtie2_index/s_cer_r64_4_1 \
-    #         -U filtered_non_rrna_reads_${srr}.fastq \
-    #         -S aligned_reads_${srr}.sam
+    echo "Running quality check report on non_rrna_reads_${srr}.fastq"
+    fastqc -q -t 8 filtered_non_rrna_reads_${srr}.fastq
 
 
-    # # Filter Unmapped Reads and convert SAM to BAM
-    # samtools view -bS aligned_reads_${srr}.sam > aligned_reads_${srr}.bam
-    # samtools sort aligned_reads_${srr}.bam -o sorted_aligned_reads_${srr}.bam
-    # samtools index sorted_aligned_reads_${srr}.bam
-    # samtools view -b -F 4 sorted_aligned_reads_${srr}.bam > filtered_aligned_reads_${srr}.bam
+
+    ########################
+    # Step 3
+    ########################
+    echo "4. Alignment and quantification of non rRNA reads"
+
+    echo "Aligning against s_cerevisiae_R64 using bowtie 2"
+
+    # Build updated reference genome RF64-4-1
+    # bowtie2-build /Users/scampione/data/s_cer_r64_4_1_bowtie2_index/GCF_000146045.2_R64_genomic.fna \
+    #               /Users/scampione/data/s_cer_r64_4_1_bowtie2_index/s_cer_r64_4_1
+
+    bowtie2 -x /Users/scampione/data/s_cer_r64_4_1_bowtie2_index/s_cer_r64_4_1 \
+            -U filtered_non_rrna_reads_${srr}.fastq \
+            -S aligned_reads_${srr}.sam
+
+
+    # Filter Unmapped Reads and convert SAM to BAM
+    samtools view -bS aligned_reads_${srr}.sam > aligned_reads_${srr}.bam
+    samtools sort aligned_reads_${srr}.bam -o sorted_aligned_reads_${srr}.bam
+    samtools index sorted_aligned_reads_${srr}.bam
+    samtools view -b -F 4 sorted_aligned_reads_${srr}.bam > filtered_aligned_reads_${srr}.bam
 
 
     echo "Quantification:"
-    
-    echo "Using featureCounts"
-    featureCounts -T 1 -g gene_name -s 1 -Q 10 \
-                -a /Users/scampione/data/Blevins_Tavella_etal_Scer_transcriptome.gtf \
-                -o quality_threshold_10_yes_stranded_Blevins_Tavella_gtf_featureCounts_${srr}_full.txt \
-                filtered_aligned_reads_${srr}.bam
 
+    echo "Using Blevins_Tavella annotation file stranded=yes"
+    htseq-count -f bam -m intersection-strict \
+                -s yes \
+                -q \
+                -i gene_name \
+                filtered_aligned_reads_${srr}.bam \
+                /Users/scampione/data/Blevins_Tavella_etal_Scer_transcriptome.gtf \
+                > yes_stranded_Blevins_Tavella_gtf_counts_${srr}_full.txt
 
-
-    # echo "Using Blevins_Tavella annotation file stranded=yes"
-    # htseq-count -f bam -m intersection-strict \
-    #             -s yes \
-    #             -q \
-    #             -i gene_name \
-    #             filtered_aligned_reads_${srr}.bam \
-    #             /Users/scampione/data/Blevins_Tavella_etal_Scer_transcriptome.gtf \
-    #             > yes_stranded_Blevins_Tavella_gtf_counts_${srr}_full.txt
-
-    # echo "Using Blevins_Tavella annotation file stranded=yes, adjusted quality threshold to 0"
-    # htseq-count -f bam -m intersection-strict --minaqual 0 \
-    #             -s yes \
-    #             -q \
-    #             -i gene_name \
-    #             filtered_aligned_reads_${srr}.bam \
-    #             /Users/scampione/data/Blevins_Tavella_etal_Scer_transcriptome.gtf \
-    #             > quality_threshold_0_yes_stranded_Blevins_Tavella_gtf_counts_${srr}_full.txt
-
-    # echo "Using Blevins_Tavella annotation file stranded=yes, adjusted quality threshold to 1"
-    # htseq-count -f bam -m intersection-strict --minaqual 1 \
-    #             -s yes \
-    #             -q \
-    #             -i gene_name \
-    #             filtered_aligned_reads_${srr}.bam \
-    #             /Users/scampione/data/Blevins_Tavella_etal_Scer_transcriptome.gtf \
-    #             > quality_threshold_1_yes_stranded_Blevins_Tavella_gtf_counts_${srr}_full.txt
-
-
-    # echo "Using Blevins_Tavella annotation file stranded=yes, adjusted quality threshold to 0, overlap mode=union"
-    # htseq-count -f bam -m union --minaqual 0 \
-    #             -s yes \
-    #             -q \
-    #             -i gene_name \
-    #             filtered_aligned_reads_${srr}.bam \
-    #             /Users/scampione/data/Blevins_Tavella_etal_Scer_transcriptome.gtf \
-    #             > union_quality_threshold_0_yes_stranded_Blevins_Tavella_gtf_counts_${srr}_full.txt
-
-
-    # echo "Subsampling"
-    # samtools view -b -s 0.4 -o half_subsampled_aligned_reads_${srr}.bam filtered_aligned_reads_${srr}.bam
-
-    # echo "Subsampled: Using Blevins_Tavella annotation file stranded=yes"
-    # htseq-count -f bam -m intersection-strict \
-    #             -s yes \
-    #             -q \
-    #             -i gene_name \
-    #             half_subsampled_aligned_reads_${srr}.bam \
-    #             /Users/scampione/data/Blevins_Tavella_etal_Scer_transcriptome.gtf \
-    #             > yes_stranded_Blevins_Tavella_gtf_counts_${srr}_subset.txt
-
-    
 
                 
 
--- rna_seq_pipeline.sh
+++ rna_seq_pipeline.sh
@@ -53,78 +53,56 @@ while IFS= read -r srr; do
     
     
 
-    # ########################
-    # # Step 2
-    # ########################
-    # echo "2. Trim adapters"
+    ########################
+    # Step 2
+    ########################
+    echo "2. Trim adapters"
 
-    # # trim and filter using default parameters
-    # trimmomatic PE \
-    #     -phred33 \
-    #     -threads 8 \
-    #     ${srr}_1.fastq ${srr}_2.fastq \
-    #     ${srr}_1_paired.fastq ${srr}_1_unpaired.fastq \
-    #     ${srr}_2_paired.fastq ${srr}_2_unpaired.fastq \
-    #     ILLUMINACLIP:/Users/scampione/anaconda3/pkgs/trimmomatic-0.39-hdfd78af_2/share/trimmomatic-0.39-2/adapters/TruSeq3-PE.fa:2:30:10 \
-    #     LEADING:3 TRAILING:3 \
-    #     SLIDINGWINDOW:4:15 MINLEN:36
+    # trim and filter using default parameters
+    trimmomatic PE \
+        -phred33 \
+        -threads 8 \
+        ${srr}_1.fastq ${srr}_2.fastq \
+        ${srr}_1_paired.fastq ${srr}_1_unpaired.fastq \
+        ${srr}_2_paired.fastq ${srr}_2_unpaired.fastq \
+        ILLUMINACLIP:/Users/scampione/anaconda3/pkgs/trimmomatic-0.39-hdfd78af_2/share/trimmomatic-0.39-2/adapters/TruSeq3-PE.fa:2:30:10 \
+        LEADING:3 TRAILING:3 \
+        SLIDINGWINDOW:4:15 MINLEN:36
         
 
-    # # quality check on trimmed reads
-    # fastqc ${srr}_1_paired.fastq ${srr}_2_paired.fastq
+    # quality check on trimmed reads
+    fastqc ${srr}_1_paired.fastq ${srr}_2_paired.fastq
 
 
-    # ########################
-    # # Step 3
-    # ########################
-    # echo "3. Alignment and quantification"
+    ########################
+    # Step 3
+    ########################
+    echo "3. Alignment and Quantification"
     
-    # # align against S Cer R64 (ver 4.1) genome assembly
-    # echo "Aligning against s_cerevisiae_R64 4.1 using bowtie 2"
-    # bowtie2 -x /Users/scampione/data/s_cer_r64_4_1_bowtie2_index/s_cer_r64_4_1 \
-    #     -1 ${srr}_1_paired.fastq \
-    #     -2 ${srr}_2_paired.fastq \
-    #     -S ${srr}_aligned_reads.sam
+    # align against S Cer R64 (ver 4.1) genome assembly
+    echo "Aligning against s_cerevisiae_R64 4.1 using bowtie 2"
+    bowtie2 -x /Users/scampione/data/s_cer_r64_4_1_bowtie2_index/s_cer_r64_4_1 \
+        -1 ${srr}_1_paired.fastq \
+        -2 ${srr}_2_paired.fastq \
+        -S ${srr}_aligned_reads.sam
 
 
 
-    # # convert SAM to BAM, sort by name (so pairs are together), and filter
-    # samtools view -bS ${srr}_aligned_reads.sam > ${srr}_aligned_reads.bam
-    # samtools sort -n ${srr}_aligned_reads.bam -o ${srr}_sorted_aligned_reads.bam
-    # samtools view -b -F 4 ${srr}_sorted_aligned_reads.bam > ${srr}_filtered_aligned_reads.bam
+    # convert SAM to BAM, sort by name (so pairs are together), and filter
+    samtools view -bS ${srr}_aligned_reads.sam > ${srr}_aligned_reads.bam
+    samtools sort -n ${srr}_aligned_reads.bam -o ${srr}_sorted_aligned_reads.bam
+    samtools view -b -F 4 ${srr}_sorted_aligned_reads.bam > ${srr}_filtered_aligned_reads.bam
 
 
-    # # quantification
-    # Mode: intersection-strict, min qual: 10
-    # htseq-count -f bam -m intersection-strict \
-    #             -s reverse \
-    #             -q \
-    #             -i gene_name \
-    #             ${srr}_filtered_aligned_reads.bam \
-    #             /Users/scampione/data/Blevins_Tavella_etal_Scer_transcriptome.gtf \
-    #             > ${srr}_gene_counts.txt
-
-    # Mode: intersection-strict, min qual: 0
-    htseq-count -f bam -m intersection-strict --minaqual 0 \
+    # Quantification
+    Mode: intersection-strict, min qual: 10
+    htseq-count -f bam -m intersection-strict \
                 -s reverse \
                 -q \
                 -i gene_name \
                 ${srr}_filtered_aligned_reads.bam \
                 /Users/scampione/data/Blevins_Tavella_etal_Scer_transcriptome.gtf \
-                > quality_thresh_0_${srr}_gene_counts.txt
-
-    # Mode: Union, Min qual: 0
-    # htseq-count -f bam -m union --minaqual 0 \
-    #             -s reverse \
-    #             -q \
-    #             -i gene_name \
-    #             ${srr}_filtered_aligned_reads.bam \
-    #             /Users/scampione/data/Blevins_Tavella_etal_Scer_transcriptome.gtf \
-    #             > union_thesh_0_${srr}_gene_counts.txt
-    
-
-    # filtered_read_count=$(samtools view -c ${srr}_filtered_aligned_reads.bam)
-    # echo "Filtered read count for ${srr}: ${filtered_read_count}"
+                > ${srr}_gene_counts.txt
 
 
     echo "End of analysis of ${srr}"
